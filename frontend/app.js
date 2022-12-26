286d9f35e4b59f8caaa4e42896756efbddeb4680
ahmed-Elnewery <ahmed.elnewery@boostiny.com>
2022-12-26 13:13:30 +0200

    realtime using http2

---
+++ frontend/app.js
@@ -0,0 +1,79 @@
+const chat = document.getElementById("chat");
+const msgs = document.getElementById("msgs");
+const presence = document.getElementById("presence-indicator");
+
+// this will hold all the most recent messages
+let allChat = [];
+
+chat.addEventListener("submit", function (e) {
+    e.preventDefault();
+    postNewMsg(chat.elements.user.value, chat.elements.text.value);
+    chat.elements.text.value = "";
+});
+
+async function postNewMsg(user, text) {
+    const data = {
+        user,
+        text,
+    };
+
+    // request options
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+
+    // send POST request
+    // we're not sending any json back, but we could
+    await fetch("/msgs", options);
+}
+
+async function getNewMsgs() {
+    // implementation
+    let reader;
+    const utfDecoder = new TextDecoder("utf-8")
+    try {
+        const res = await fetch("/msgs")
+        reader = res.body.getReader()
+        
+
+    } catch (error) {
+        console.error("connection error", e)
+    }
+    presence.innerText = '🟢'
+    let readResponse;
+    let done;
+    do {
+        try {
+            readResponse = await reader.read()
+        } catch (error) {
+            console.log("reader fails")
+            presence.innerText = "🔴"
+            return;
+        }
+        const chunk = utfDecoder.decode(readResponse.value, { stream: true })
+        done = readResponse.done
+        if (chunk) {
+            let json = JSON.parse(chunk)
+            allChat = json.msg
+            render()
+        }
+    } while (!done);
+
+
+}
+
+function render() {
+    const html = allChat.map(({ user, text, time, id }) =>
+        template(user, text, time, id)
+    );
+    msgs.innerHTML = html.join("\n");
+}
+
+const template = (user, msg) =>
+    `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
+
+getNewMsgs();
